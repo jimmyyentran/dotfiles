58b1f278cb241e496b019c5afe362bc7b89dd463
Jimmy Tran <jtran064@ucr.edu>
2015-12-25 13:09:16 -0800

    intelliJ idea bin folder

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -4,6 +4,7 @@ alias zrc='~/.dotfiles/zsh/'
 # alias eclimd='eclimd -Dosgi.instance.area.default=@user.home/Java &'
 alias zr='nvim ~/.zshrc'
 alias za='nvim ~/.dotfiles/zsh/aliases.zsh'
+alias lc='nvim ~/.dotfiles/zsh/local.zsh'
 alias vr='nvim ~/.nvimrc'
 alias lt='ls -lAhtr'
 alias lz='ls -lAS'
@@ -12,3 +13,4 @@ alias q="exit"
 alias dl="cd ~/Downloads"
 alias dt="cd ~/Desktop"
 alias make='nocorrect make' #stop make from autocorrecting things
+alias sag="sudo apt-get"
