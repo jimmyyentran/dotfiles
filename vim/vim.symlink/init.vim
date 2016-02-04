994ab89879926ac866a7d714b461df6ca80b280f
jtran064 <jtran064@ucr.edu>
2016-02-03 19:38:19 -0800

    remove unnecessary files

--- vim/vim.symlink/init.vim
+++
@@ -1,1 +0,0 @@
-/home/jt/.vimrc
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,17 +1,23 @@
+# EDITOR=vim
 alias reload!='. ~/.zshrc'
-alias v='nvim'
+alias v='$EDITOR'
 alias zrc='~/.dotfiles/zsh/'
 # alias eclimd='eclimd -Dosgi.instance.area.default=@user.home/Java &'
-alias zr='nvim ~/.zshrc'
-alias za='nvim ~/.dotfiles/zsh/aliases.zsh'
-alias lc='nvim ~/.dotfiles/zsh/local.zsh'
-alias vr='nvim ~/.vimrc'
+alias zr='$EDITOR ~/.zshrc'
+alias za='$EDITOR ~/.dotfiles/zsh/aliases.zsh'
+alias lc='$EDITOR ~/.dotfiles/zsh/local.zsh'
+alias vr='$EDITOR ~/.vimrc'
 alias lt='ls -lAhtr'
 alias lz='ls -lAS'
 alias gno='gnome-open'
 alias q="exit"
 alias dl="cd ~/Downloads"
 alias dt="cd ~/Desktop"
+alias doc="cd ~/Documents"
 alias make='nocorrect make' #stop make from autocorrecting things
-alias sag="sudo apt-get"
+# alias sag="sudo apt-get"
 alias rs="killall plasmashell ; kstart plasmashell &>/dev/null" #restart KDE5
+alias szsh="source ~/.zshrc" # source zsh
+alias python=python3
+
+export PYTHONPATH=$PYTHONPATH:/home/jt2/Workspace/python/gdata-python-client/src
