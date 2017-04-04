c286ffa0d278fb0f947fd8cbe610c8abe48be5d9
Jimmy Tran <jtran064@ucr.edu>
2017-04-03 20:22:07 -0700

    jupyter js

---
+++ jupyter/custom/custom.js
@@ -0,0 +1,15 @@
+// Configure CodeMirror
+require([
+  'nbextensions/vim_binding/vim_binding',   // depends your installation
+], function() {
+  CodeMirror.Vim.map(";", ":", "normal");
+  CodeMirror.Vim.map("<C-n>", "<Down>", "insert");
+
+  // Map jj to <Esc>
+  // CodeMirror.Vim.map("jj", "<Esc>", "insert");
+  // Swap j/k and gj/gk (Note that <Plug> mappings)
+  // CodeMirror.Vim.map("j", "<Plug>(vim-binding-gj)", "normal");
+  // CodeMirror.Vim.map("k", "<Plug>(vim-binding-gk)", "normal");
+  // CodeMirror.Vim.map("gj", "<Plug>(vim-binding-j)", "normal");
+  // CodeMirror.Vim.map("gk", "<Plug>(vim-binding-k)", "normal");
+});
