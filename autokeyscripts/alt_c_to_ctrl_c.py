2ad0b16a2b863ad453c57b30f440ac71fd8f2570
Jimmy Tran <jimmyytran@gmail.com>
2018-04-18 11:36:52 -0700

    add hotkeys

--- autokeyscripts/alt_c_to_ctrl_c.py
+++ autokeyscripts/alt_c_to_ctrl_c.py
@@ -1,2 +1,6 @@
 # Enter script code
-keyboard.send_keys("<ctrl>+c")
+winClass = window.get_active_class()
+if '.Gnome-terminal' in winClass:
+    keyboard.send_keys("<ctrl>+<shift>+c")
+else:
+    keyboard.send_keys("<ctrl>+c")
--- autokeyscripts/alt_v_to_ctrl_v.py
+++ autokeyscripts/alt_v_to_ctrl_v.py
@@ -1,2 +1,6 @@
 # Enter script code
-keyboard.send_keys("<ctrl>+v")
+winClass = window.get_active_class()
+if '.Gnome-terminal' in winClass:
+    keyboard.send_keys("<ctrl>+<shift>+v")
+else:
+    keyboard.send_keys("<ctrl>+v")
