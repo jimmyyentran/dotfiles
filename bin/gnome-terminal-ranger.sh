33444b2bde432077e97789803dbaed00e882bece
Jimmy Tran <jimmyytran@gmail.com>
2018-05-01 00:22:51 -0700

    notifications for sound, background, disable i3block sound

--- bin/gnome-terminal-ranger.sh
+++ bin/gnome-terminal-ranger.sh
@@ -1,1 +1,1 @@
-gnome-terminal --window-with-profile=ranger -e "bash -c 'printf \"\033]0;Terminal r$(wmctrl -l | grep "Terminal r"| wc -l)\007\"; ranger'"
+gnome-terminal --hide-menubar --window-with-profile=ranger -e "bash -c 'printf \"\033]0;Terminal r$(wmctrl -l | grep "Terminal r"| wc -l)\007\"; ranger'"
